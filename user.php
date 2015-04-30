83ded1f1d0b67f7a0e88b3b2ec7b2f68f938c222
Volodymyr Shalata <bigvolodumur@gmail.com>
2015-04-30 15:47:59 +0300

    add server file

--- user.php
+++ user.php
@@ -1,13 +1,15 @@
-<?php
+<?php //
 
-include_once ('PDO_Config.php');
-
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-//    print_r($_POST);exit;
-//     $data = json_decode(file_get_contents('http://localhost/www/Backbone/#/new'), true);
-}
-//print_r($data);exit;
-$allowed = array("Fname","Lname","Age"); // allowed fields
-$sql = "INSERT INTO Users SET ".pdoSet($allowed,$values);
-$stm = $pdo->prepare($sql);
-$stm->execute($values);
+//include_once ('PDO_Config.php');
+//
+//$_TEST  = array();  
+////echo $_SERVER['REQUEST_METHOD'];exit;
+//if($_SERVER['REQUEST_METHOD'] == 'POST') { 
+////    echo 1;exit;
+////    parse_str(file_get_contents('php://input'), $_TEST);  
+//}  
+//print_r($_POST);exit;
+////$allowed = array("Fname","Lname","Age"); // allowed fields
+////$sql = "INSERT INTO Users SET ".pdoSet($allowed,$values);
+////$stm = $pdo->prepare($sql);
+////$stm->execute($values);
--- users.php
+++ users.php
@@ -1,9 +1,9 @@
 <?php
 
-include_once ('PDO_Config.php');
-
-$userList = $pdo->prepare('SELECT * FROM Users WHERE id!= :id');
-$userList->execute(array('id'=> 1));
-$users = $userList->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($users);
+//include_once ('PDO_Config.php');
+//
+//$userList = $pdo->prepare('SELECT * FROM Users WHERE id!= :id');
+//$userList->execute(array('id'=> 1));
+//$users = $userList->fetchAll(PDO::FETCH_ASSOC);
+//
+//echo json_encode($users);
