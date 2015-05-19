cb2273b0789b9b4425721c946e7dff9915b2e572
Volodymyr Shalata <bigvolodumur@gmail.com>
2015-05-19 19:42:24 +0300

    End of the application. Still not working redirect

--- users.php
+++ users.php
@@ -1,19 +1,35 @@
 <?php
 
 include_once ('PDO_Config.php');
-//
+
 if($_SERVER['REQUEST_METHOD'] == 'PUT') {
-    echo "this is a put request\n";
-    parse_str(file_get_contents("php://input"),$post_vars);
-//    echo $post_vars['fruit']." is the fruit\n";
-//    echo "I want ".$post_vars['quantity']." of them\n\n";
-    var_dump($post_vars);
+    $put_data = file_get_contents("php://input");
+    $put_data = json_decode($put_data,true);
+
+    $Fname = $put_data['Fname'];
+    $Lname = $put_data['Lname'];
+    $Age = $put_data['Age'];
+    $id = $put_data['id'];
+
+    $userInfo = $pdo->prepare('UPDATE Users SET Fname = ?, Lname = ?, Age = ? WHERE id = ?');
+    $userInfo->execute(array($Fname, $Lname, $Age, $id));
 }
+
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $userList = $pdo->prepare('SELECT * FROM Users WHERE id!= :id');
-    $userList->execute(array('id'=> 1));
-    $users = $userList->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($users);
+    $url = explode('/',$_SERVER['REQUEST_URI']);
+    //todo rewrite this check
+    if(count($url) == 5){
+        $id = $url[count($url)-1];
+        $userInfo = $pdo->prepare('SELECT * FROM Users WHERE id= :id');
+        $userInfo->execute(array('id'=> $id));
+        $user = $userInfo->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($user);
+    }else{
+        $userList = $pdo->prepare('SELECT * FROM Users WHERE id!= :id');
+        $userList->execute(array('id'=> 1));
+        $users = $userList->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($users);
+    }
 }
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $json = file_get_contents('php://input');
@@ -25,6 +41,21 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     
     //todo Use prepeare statement in PDO insert
     $result = $pdo->exec("INSERT INTO Users (Fname, Lname, Age) VALUES('$Fname', '$Lname', '$Age')");
+//    header("HTTP/1.1 200 OK");
+//    exit;
+    header("HTTP/1.1 200 OK");
+
+}
+if($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    //$json = file_get_contents('php://input');
+    //$obj = json_decode($json,true);
+    $url = explode('/',$_SERVER['REQUEST_URI']);
+    $id = $url[count($url)-1];
+
+    $sql = "DELETE FROM Users WHERE id =  :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
 
 }
 
