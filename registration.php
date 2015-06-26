c403deb4dedfa8ae913bc44a3282be4461d3a851
Volodymyr Shalata <bigvolodumur@gmail.com>
2015-06-26 16:30:07 +0300

    Registration complete

--- registration.php
+++ registration.php
@@ -4,8 +4,18 @@ include_once ('PDO_Config.php');
 include('constants.php');
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $json = file_get_contents('php://input');
-    var_dump($json);exit;
+    $reg_data = json_decode(file_get_contents('php://input'),true);
+
+    //var_dump($reg_data);exit;
+
+    $first_name = $reg_data['first_name'];
+    $last_name = $reg_data['last_name'];
+    $password = $reg_data['password'];
+    $email = $reg_data['email'];
+    $age = $reg_data['age'];
+
+    $result = $pdo->exec("INSERT INTO Users (Fname, Lname, Age) VALUES('$first_name', '$last_name', '$age')");
+
 }
 
 ?>
